a2167b3f9f75c6ebdabad44995cbe634ef1c2d40
Jose Carlos <josecarlos231998@gmail.com>
2021-12-30 22:57:23 -0700

    register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,13 +25,18 @@ const routes: Routes = [
   {
     path: 'menu',
     loadChildren: () => import('./modules/menu/menu.module').then( m => m.MenuPageModule)
-  },  {
+  },
+  {
     path: 'profile',
     loadChildren: () => import('./modules/profile/profile.module').then( m => m.ProfilePageModule)
   },
   {
     path: 'last-movements',
     loadChildren: () => import('./modules/last-movements/last-movements.module').then( m => m.LastMovementsPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./modules/register/register.module').then( m => m.RegisterPageModule)
   }
 
  
